d35339266da7e43d9637a27503669649a80d3933
JuanPaloudeComasema <jpaldec@edu.upv.es>
2024-11-28 18:03:55 +0100

    Añadida implementación de la clase BSNode

---
+++ BSNode.h
@@ -0,0 +1,18 @@
+#ifndef BSNODE_H
+#define BSNODE_H
+
+#include <ostream>
+
+template <typename T> 
+class BSNode {
+    public:
+        // miembros públicos
+	T elem;
+	BSNode<T>* left;
+	BSNode<T>* right;
+	BSNode(T elem, BSNode<T>* left=nullptr, BSNode<T>* right=nullptr);
+	friend std::ostream& operator<<(std::ostream &out, const BSNode<T> &bsn);
+    
+};
+
+#endif
