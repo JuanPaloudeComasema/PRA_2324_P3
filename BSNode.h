aaed12fb0dda362376b3b970c96ac4f2b7324ba8
JuanPaloudeComasema <jpaldec@edu.upv.es>
2024-12-11 18:37:52 +0100

    Añadida implementación de la clase BSNode

--- BSNode.h
+++ BSNode.h
@@ -3,16 +3,27 @@
 
 #include <ostream>
 
-template <typename T> 
-class BSNode {
-    public:
-        // miembros públicos
-	T elem;
-	BSNode<T>* left;
-	BSNode<T>* right;
-	BSNode(T elem, BSNode<T>* left=nullptr, BSNode<T>* right=nullptr);
-	friend std::ostream& operator<<(std::ostream &out, const BSNode<T> &bsn);
-    
+using namespace std;
+
+template <typename T>
+class BSNode{
+ public:
+  T elem;                //Elemento de tipo T almacenado en el Nodo
+  BSNode<T>* left;       //Puntero al Nodo sucesor izquierdo
+  BSNode<T>* right;      //Puntero al Nodo sucesor derecho
+
+  //Método constructor -> Inicializa variables (nulos por defecto)
+  BSNode(T elem, BSNode<T>* left = nullptr, BSNode<T>* right = nullptr){
+    this -> elem = elem;
+    this -> left = left;
+    this -> right = right;
+  }
+
+  //Imprime el nodo por pantalla
+  friend ostream&operator<<(ostream&out, const BSNode<T>&bsn){
+    out << bsn.elem;
+    return out;
+  }
 };
 
 #endif
