b3111ef338873545b9b889586312d2ce00f4ae36
JuanPaloudeComasema <jpaldec@edu.upv.es>
2024-12-11 18:32:04 +0100

    Añadida implementación de la clase TableEntry

--- TableEntry.h
+++ TableEntry.h
@@ -1,34 +1,74 @@
 #ifndef TABLEENTRY_H
-#define TABLEENTRY_H
+#define	TABLEENTRY_H
 
 #include <string>
 #include <ostream>
 
-template <typename V> 
-class TableEntry {
-    public:
-	   //mienbros publicos
-	   std::string key;
-	   V value;
+using namespace std;
 
-	   TableEntry(std::string key, V value): key(key), value(value) {}
+template <typename V>
+class TableEntry{            //Clase que representa los pares clave->valor
+	public:
+		string key;
+		V value;
 
-	   TableEntry(std::string key) : key(key), value(V()) {}
+		//Método constructor de par Clave->Valor
+		TableEntry(string key, V value){
+			this -> key = key;
+			this -> value = value;
+		}
 
-	   TableEntry(): key(""), value(V()) {}
+		//Crea una entrada solo con clave (sin valor)
+		TableEntry(string key){
+			this -> key = key;
+		}
 
-	   friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
-		   return &te1.key == &te2.key;
-	   }
-	  friend bool operator!=(TableEntry<V> &te1, const TableEntry<V> &te2){
-		return	&te1.key != &te2.key;
-	   }
-	
-	  friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
-		  out<< "Key: " << &te.key << "Value: " << &te.value;
-		  return out;
-	  } 
-		   
+		//Crea una entrada con la clave siendo una cadena vacía (sin valor)
+		TableEntry(){
+			key = "";
+		}
+
+		//Devuelve 1 si las claves son iguales o 0 si no lo son (es función bool)
+		friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			if(te1.key == te2.key){
+				return 1;
+			}else{
+				return 0;
+			}
+		}
+
+		//Devuelve 1 si las claves son diferentes o 0 si no lo son (es función bool)
+		friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			if(te1.key != te2.key){
+				return 1;
+			}else{
+				return 0;
+			}
+		}
+
+		//Devuelve 1 clave 1 > clave 2, si no lo es, devuelve 0 (Para Árboles Binarios)
+		friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2){
+		        if(te1.key < te2.key){
+				return 1;
+			}else{
+				return 0;
+			}
+		}
+
+		//Devuelve 1 clave 1 > clave 2, si no lo es, devuelve 0 (Para Árboles Binarios)
+		friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2){
+		        if(te1.key > te2.key){
+				return 1;
+			}else{
+				return 0;
+			}
+		}
+
+		//Imprime por pantalla la Clave y el Valor del "par" (te)
+		friend ostream&operator<<(ostream &out, const TableEntry<V> &te){
+			out << "(Clave -> '" << te.key << "' => Valor ->  " << te.value << ")";
+			return out;
+		}
 };
 
 #endif
