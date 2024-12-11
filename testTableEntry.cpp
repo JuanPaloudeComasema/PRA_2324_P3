3cfea8d0e6b3ba23f7e924ec4261b602c8265c5a
JuanPaloudeComasema <jpaldec@edu.upv.es>
2024-12-11 18:33:12 +0100

    Añadido Makefile y código de test de la clase TableEntry

--- testTableEntry.cpp
+++ testTableEntry.cpp
@@ -2,6 +2,7 @@
 #include "TableEntry.h"
 
 int main(){
+
     std::cout << std::boolalpha; // configuramos cout para mostrar true/false en lugar de 0/1.
 
     TableEntry<int> e1("Catorze", 14);
@@ -11,9 +12,9 @@ int main(){
     std::cout << "e1: " << e1 << std::endl;
     std::cout << "e2: " << e2 << std::endl;
     std::cout << "e3: " << e3 << std::endl;
-    
+
     std::cout << std::endl;
-    
+
     std::cout << "e1 == e2 ? " << (e1 == e2) << std::endl;
     std::cout << "e1 != e2 ? " << (e1 != e2) << std::endl;
     std::cout << std::endl;
